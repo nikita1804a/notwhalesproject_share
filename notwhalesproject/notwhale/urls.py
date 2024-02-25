3b09e518d4e1f216a367a8b508b6ed506666fff1
Notawhale <Nikita1804a@mail.ru>
2024-02-25 18:31:31 +0300

    commit_2

--- notwhalesproject/notwhale/urls.py
+++ notwhalesproject/notwhale/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.helloworld, name='helloworld'),
+    path("one/", views.func_1, name='func_1'),
+    path("two/", views.func_2, name='func_2'),
 ]
