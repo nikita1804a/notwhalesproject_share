2e198bc24809e98c4f4a0a8e8604f41f6ac0b05f
Notawhale <Nikita1804a@mail.ru>
2024-03-03 18:30:54 +0300

    commit 3

--- notwhalesproject/notwhale/urls.py
+++ notwhalesproject/notwhale/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.helloworld, name='helloworld'),
-    path("one/", views.func_1, name='func_1'),
-    path("two/", views.func_2, name='func_2'),
+    path("", views.base, name='base'),
 ]
--- notwhalesproject/notwhale/views.py
+++ notwhalesproject/notwhale/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'helloworld.html')
-
-def func_1(request):
-    return render(request, 'one.html')
-
-def func_2(request):
-    return render(request, 'two.html')
+def base(request):
+    return render(request, 'base.html')
