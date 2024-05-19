8f4a96675335aba89833fd093ea715431f24388f
notwhale <Nikita1804a@mail.ru>
2024-05-19 18:25:25 +0300

    commit 11

--- notwhalesproject/notwhale/urls.py
+++ notwhalesproject/notwhale/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.base, name='base'),
+    path("", views.index, name='index'),
+    path('login/', views.login, name='login')
 ]
--- notwhalesproject/notwhale/views.py
+++ notwhalesproject/notwhale/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def base(request):
-    return render(request, 'base.html')
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
